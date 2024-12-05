7a8cbd12e423e925800ae410f823942875d24989
Joseph Lumbley <joelumbley16@gmail.com>
2024-12-05 13:16:00 -0600

    Clean up

--- Timer/Form1.vb
+++ Timer/Form1.vb
@@ -1,6 +1,18 @@
 ﻿' Timer⏳
 
+' This application is designed to provide an intuitive and user-friendly
+' countdown timer experience.
 
+' Features
+
+'  Countdown Timer: Easily set and track countdowns for various activities.
+'  Pause/Resume Functionality: Take control of your timing with the ability
+'  to pause and resume as needed.
+'  Customizable Duration: Set your desired timer duration with ease.
+'  Sound Effects: Enjoy auditory alerts with integrated sound effects when
+'  the timer completes.
+'  Responsive UI: The graphical interface adapts seamlessly to different window
+'  sizes, ensuring a consistent user experience across devices.
 
 ' MIT License
 ' Copyright(c) 2024 Joseph W. Lumbley
